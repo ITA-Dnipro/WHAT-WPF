286b6dc96bcdac9aa31babf40587ba4a9ad16065
bogdan24bondar@gmail.com <bogdan24bondar@gmail.com>
2021-05-07 17:34:22 +0300

    Create CellButton class

---
+++ Battleship_WPF/CellButton.cs
@@ -0,0 +1,15 @@
+﻿using BattleshipLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Battleship_WPF
+{
+    class CellButton : Button
+    {
+        public Position Coord { get; set; }
+    }
+}
